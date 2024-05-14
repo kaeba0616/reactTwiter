5c4d1aa67ff8c06aed867d60fa08a64a1f4c983d
kaeba0616 <alstjq1012@gmail.com>
2024-05-14 16:46:07 +0900

    2.3 firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDDn1n1ghrn6ZJ7fG42phzNepwBoCUMj10",
+  authDomain: "lwitter-reloaded.firebaseapp.com",
+  projectId: "lwitter-reloaded",
+  storageBucket: "lwitter-reloaded.appspot.com",
+  messagingSenderId: "605188862799",
+  appId: "1:605188862799:web:7aa4822980587c6f504b3a",
+};
+
+const app = initializeApp(firebaseConfig);

33ea1f954b9d96980b79863647f5ee32b2e0e5ad
kaeba0616 <alstjq1012@gmail.com>
2024-05-14 16:52:17 +0900

    #3.0 Setup(Authentication)

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDDn1n1ghrn6ZJ7fG42phzNepwBoCUMj10",
   authDomain: "lwitter-reloaded.firebaseapp.com",
@@ -9,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);

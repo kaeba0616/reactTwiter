dd44f9349904126129efb3520458b887d8951f5c
kaeba0616 <alstjq1012@gmail.com>
2024-05-15 17:23:20 +0900

    #3.5 Github Login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -11,6 +11,7 @@ import {
   Wrapper,
   Form,
 } from "../components/auth-components";
+import GithubButton from "../components/github-btn";
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -80,6 +81,7 @@ export default function CreateAccount() {
         Don't have an account?{" "}
         <Link to="/create-account">Create one &rarr;</Link>
       </Switcher>
+      <GithubButton />
     </Wrapper>
   );
 }
